131e402f3797508b7a5945fe528238274bc68924
Your Name <abhishekdd71@gmail.com>
2026-03-29 16:07:49 +0530

    Program 7: Improved Django admin UI (search, list display, ManyToMany filter)

--- myproject/app4/admin.py
+++ myproject/app4/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import Student, Course
 
-admin.site.register(Student)
-admin.site.register(Course)
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email')
+    search_fields = ('first_name', 'last_name', 'email')
+
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
+    filter_horizontal = ('students',)
+
+
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Course, CourseAdmin)
