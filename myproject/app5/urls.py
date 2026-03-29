b9c9e046c6daee8183f9530ec9f0a61f86d52c5a
Your Name <abhishekdd71@gmail.com>
2026-03-29 19:18:00 +0530

    program 9(data export throught csv and pdf feature added)

--- myproject/app5/urls.py
+++ myproject/app5/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import StudentListView, StudentDetailView
+from .views import generate_csv, generate_pdf
 
 urlpatterns = [
     path('students/', StudentListView.as_view(), name='student_list'),
     path('students/<int:pk>/', StudentDetailView.as_view(), name='student_detail'),
+    path('students/csv/', generate_csv, name='generate_csv'),
+    path('students/pdf/', generate_pdf, name='generate_pdf'),
 ]
+
--- myproject/app5/views.py
+++ myproject/app5/views.py
@@ -1,5 +1,9 @@
-from django.views.generic import ListView, DetailView
+import csv
+from django.http import HttpResponse, FileResponse
 from app4.models import Student
+from io import BytesIO
+from reportlab.pdfgen import canvas
+from django.views.generic import ListView, DetailView
 
 
 class StudentListView(ListView):
@@ -12,3 +16,31 @@ class StudentDetailView(DetailView):
     model = Student
     template_name = 'app5/student_detail.html'
     context_object_name = 'student'
+def generate_csv(request):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="students.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow(['First Name', 'Last Name', 'Email'])
+
+    students = Student.objects.all()
+    for student in students:
+        writer.writerow([student.first_name, student.last_name, student.email])
+
+    return response
+def generate_pdf(request):
+    buffer = BytesIO()
+    p = canvas.Canvas(buffer)
+
+    students = Student.objects.all()
+
+    y = 800
+    for student in students:
+        text = f"{student.first_name} {student.last_name} - {student.email}"
+        p.drawString(50, y, text)
+        y -= 20
+
+    p.save()
+    buffer.seek(0)
+
+    return FileResponse(buffer, as_attachment=True, filename='students.pdf')
